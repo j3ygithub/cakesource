12d2fa82c85174ffcf735c60002a14ac19c00438
Jimmy Lin <b00502013@gmail.com>
2022-09-10 11:39:39 +0800

    Delete main.py

--- images/portfolios/ctdb/main.py
+++
@@ -1,9 +0,0 @@
-import pathlib
-
-
-base_dir = pathlib.Path(__file__).resolve().parent
-for file in base_dir.iterdir():
-    fullname = file.name
-    if fullname.startswith("ctdb-"):
-        name = fullname.split("ctdb-")[-1]
-    file.rename(file.with_name(name))
